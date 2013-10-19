c1aaf5926221af0a4cddc1bff63e04a54b535ef7
Eric Ebbesen <Eric.Ebbesen@govdelivery.com>
2013-10-19 10:22:52 -0500

    Porting CssInliner

---
+++ src/main/java/com/humegatech/inliner/CssInliner.java
@@ -0,0 +1,128 @@
+package com.humegatech.inliner;
+
+import java.nio.charset.Charset;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.phloc.css.ECSSVersion;
+import com.phloc.css.ICSSWriterSettings;
+import com.phloc.css.decl.CSSDeclaration;
+import com.phloc.css.decl.CSSSelector;
+import com.phloc.css.decl.CSSStyleRule;
+import com.phloc.css.decl.CascadingStyleSheet;
+import com.phloc.css.reader.CSSReader;
+import com.phloc.css.writer.CSSWriterSettings;
+
+public class CssInliner {
+    private static final Charset CHARSET = Charset.forName("UTF-8");
+    private static final ECSSVersion CSS_VERSION = ECSSVersion.CSS30;
+    // Using 0, but value doesn't matter for us since we're not indenting (or
+    // supporting indented CSS yet)
+    private static final int INDENT_LEVEL = 0;
+
+    /*
+     * Passing true for the second parameter causes the writer to strip all
+     * whitespace from the output: perfect for inlining
+     */
+    private static final ICSSWriterSettings WRITER_SETTINGS = new CSSWriterSettings(ECSSVersion.CSS30, true);
+
+    /**
+     * Will take CSS and a class selector and will produce the css inlined
+     * 
+     * @param css
+     *            CSS
+     * @param cssClassSelector
+     *            class for which to inline the css
+     * @return String representing inlined css
+     */
+    public static String inlineCssClass(final String css, final String cssClassSelector) {
+        final String classDeclarations = getDeclarationsForClassSelector(cssClassSelector, css);
+
+        return (StringUtils.isNotBlank(classDeclarations) ? classDeclarations : null);
+    }
+
+    /**
+     * Will take CSS and an id selector and will produce a tag the css inlined
+     * 
+     * @param css
+     *            CSS
+     * @param cssIdSelector
+     *            id for which to inline the css
+     * @return String representing inlined css
+     */
+    public static String inlineCssId(final String css, final String cssIdSelector) {
+        final String idDeclarations = getDeclarationsForIdSelector(cssIdSelector, css);
+
+        return (StringUtils.isNotBlank(idDeclarations) ? idDeclarations : "");
+    }
+
+    protected static CascadingStyleSheet parseCss(final String css) {
+        final CascadingStyleSheet aCss = CSSReader.readFromString(css, CHARSET, CSS_VERSION);
+
+        // TODO: null check and throw error if null?
+        return aCss;
+    }
+
+    /**
+     * Return the declarations for a CSS class selector as a String suitable for
+     * inlining (newlines, extra spaces, etc. removed). Assumes '.' starts class
+     * selector name so will append it if missing.
+     * 
+     * @param classSelectorName
+     *            CSS selector element name
+     * @param css
+     *            CSS document
+     * @return declarations for selector stripped of whitespace
+     */
+    protected static String getDeclarationsForClassSelector(final String classSelectorName, final String css) {
+        String declarationString = "";
+
+        final String localClassSelectorName = classSelectorName.startsWith(".") ? classSelectorName : "."
+                + classSelectorName;
+
+        for (final CSSStyleRule styleRule : parseCss(css).getAllStyleRules()) {
+            for (final CSSSelector selector : styleRule.getAllSelectors()) {
+                if (selector.getAsCSSString(WRITER_SETTINGS, INDENT_LEVEL).equalsIgnoreCase(localClassSelectorName)) {
+                    for (final CSSDeclaration declaration : styleRule.getAllDeclarations()) {
+                        declarationString += declaration.getAsCSSString(WRITER_SETTINGS, INDENT_LEVEL) + ";";
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        return declarationString;
+    }
+
+    /**
+     * Return the declarations for a CSS id selector as a String suitable for
+     * inlining (newlines, extra spaces, etc. removed). Assumes '#' starts id
+     * selector name so will append it if missing.
+     * 
+     * @param idSelectorName
+     *            CSS selector element name
+     * @param css
+     *            CSS document
+     * @return declarations for selector stripped of whitespace
+     */
+    protected static String getDeclarationsForIdSelector(final String idSelectorName, final String css) {
+        String declarationString = "";
+
+        final String localIdSelectorName = idSelectorName.startsWith("#") ? idSelectorName : "#" + idSelectorName;
+
+        for (final CSSStyleRule styleRule : parseCss(css).getAllStyleRules()) {
+            for (final CSSSelector selector : styleRule.getAllSelectors()) {
+                if (selector.getAsCSSString(WRITER_SETTINGS, INDENT_LEVEL).equalsIgnoreCase(localIdSelectorName)) {
+                    for (final CSSDeclaration declaration : styleRule.getAllDeclarations()) {
+                        declarationString += declaration.getAsCSSString(WRITER_SETTINGS, INDENT_LEVEL) + ";";
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        return declarationString;
+    }
+}
---
+++ src/test/java/com/humegatech/inliner/CssInlinerTest.java
@@ -0,0 +1,106 @@
+package com.humegatech.inliner;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class CssInlinerTest extends TestCase {
+    private static final String HTML_MESSAGE_CLASS_DECLARATION = "width: 740px; margin-bottom: 2em;";
+    private static final String HTML_MESSAGE_CLASS = ".html_message_class";
+    private static final String BANNER_CLASS_DECLARATION = "width: 700px;";
+    private static final String BANNER_CLASS = ".banner_class";
+    private static final String BANNER_ID = "#digest-banner-id";
+    private static final String BANNER_ID_DECLARATION = "width: 640px;";
+    private static final String CSS = HTML_MESSAGE_CLASS + " { " + HTML_MESSAGE_CLASS_DECLARATION + " }\n"
+            + BANNER_CLASS + " { " + BANNER_CLASS_DECLARATION + " }" + BANNER_ID + " { " + BANNER_ID_DECLARATION
+            + " } ";
+
+    @Test
+    public void testInlineCssSingleElementNoUserDefinedAttributes() {
+        final String classSelector = HTML_MESSAGE_CLASS.substring(1);
+        final String expectedHtml = stripWhitespace(HTML_MESSAGE_CLASS_DECLARATION);
+
+        assertEquals(expectedHtml, CssInliner.inlineCssClass(CSS, classSelector));
+    }
+
+    @Test
+    public void testInlineCssNoClassesInPreInlinedHtml() {
+        final String classSelector = "rss_item_feed";
+
+        assertEquals(null, CssInliner.inlineCssClass(CSS, classSelector));
+    }
+
+    @Test
+    public void testGetDeclarationsForClassSelectorClassClassNotInCss() {
+        assertEquals("", CssInliner.getDeclarationsForClassSelector("not_here", CSS));
+    }
+
+    @Test
+    public void testGetDeclarationsForIdSelector() {
+        // '#' as part of class selector
+        assertEquals(stripWhitespace(BANNER_ID_DECLARATION), CssInliner.getDeclarationsForIdSelector(BANNER_ID, CSS));
+
+        // no '#' as part of class selector
+        assertEquals(stripWhitespace(BANNER_ID_DECLARATION),
+                CssInliner.getDeclarationsForIdSelector(BANNER_ID.substring(1), CSS));
+    }
+
+    @Test
+    public void testGetDeclarationsForClassSelector() {
+        // '.' as part of id selector
+        assertEquals(stripWhitespace(HTML_MESSAGE_CLASS_DECLARATION),
+                CssInliner.getDeclarationsForClassSelector(HTML_MESSAGE_CLASS, CSS));
+        assertEquals(stripWhitespace(BANNER_CLASS_DECLARATION),
+                CssInliner.getDeclarationsForClassSelector(BANNER_CLASS, CSS));
+
+        // no '.' as part of id selector
+        assertEquals(stripWhitespace(HTML_MESSAGE_CLASS_DECLARATION),
+                CssInliner.getDeclarationsForClassSelector(HTML_MESSAGE_CLASS.substring(1), CSS));
+        assertEquals(stripWhitespace(BANNER_CLASS_DECLARATION),
+                CssInliner.getDeclarationsForClassSelector(BANNER_CLASS.substring(1), CSS));
+    }
+
+    @Test
+    public void testGetDeclarationsForClassSelectorClassMalformedCss() {
+        assertEquals("", CssInliner.getDeclarationsForClassSelector(HTML_MESSAGE_CLASS + " {"
+                + HTML_MESSAGE_CLASS_DECLARATION, CSS));
+    }
+
+    @Test
+    public void testGetDeclarationsForClassSelectorClassEmptyCss() {
+        assertEquals("", CssInliner.getDeclarationsForClassSelector(HTML_MESSAGE_CLASS, ""));
+    }
+
+    /*
+     * Not run as part of suite but helpful for figuring out what Phloc is doing
+     */
+    //    public void testPhlocCss()
+    //    {
+    //        final ICSSWriterSettings writerSettings = new CSSWriterSettings(ECSSVersion.CSS30, true);
+    //        final CascadingStyleSheet aCss = CSSReader.readFromString(CSS, Charset.forName("utf-8"), ECSSVersion.CSS30);
+    //        for (final CSSStyleRule styleRule : aCss.getAllStyleRules())
+    //        {
+    //            System.out.println(styleRule.getAsCSSString(writerSettings, 1));
+    //            for (final CSSSelector selector : styleRule.getAllSelectors())
+    //            {
+    //                System.out.println(selector.getAsCSSString(writerSettings, 1));
+    //            }
+    //
+    //            for (final CSSDeclaration declaration : styleRule.getAllDeclarations())
+    //            {
+    //                System.out.println(declaration.getAsCSSString(writerSettings, 1));
+    //            }
+    //        }
+    //    }
+
+    /**
+     * Strip all whitespace (spaces, tabs, carriage returns, line feeds, etc.)
+     * from a String
+     * 
+     * @param inputString
+     * @return corresponding String with all whitespace stripped
+     */
+    private static String stripWhitespace(final String inputString) {
+        return inputString.replaceAll("\\s", "");
+    }
+}
